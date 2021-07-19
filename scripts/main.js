22dba93b51957c4552807819d003321442a2f312
bijayagyawali70 <bijayagyawali70@gmail.com>
2021-07-20 00:16:06 +0545

    place webpage files

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc ==='images/loki-first-image.png'){
+        myImage.setAttribute('src','images/loki-second-image.png');
+    }
+    else{
+        myImage.setAttribute('src','images/loki-first-image.png');
+    }
+}
+let myButton = document.querySelector('button');
+//let myHeading = document.querySelector('h1');
+
+
+myButton.onclick = function(){
+    alert('I am Loki, of Asgard,and i am burdended with glorious purpose');
+}
+// this code is used for change user in h1 element
+// function setUserName(){
+//     let myName = prompt('Please enter your name.');
+//     if(!myName){
+//         setUserName();
+//     }
+//     localStorage.setItem('name',myName);
+//     myHeading.textContent = 'Introducing Loki to, ' + myName;
+// }
+
+// if(!localStorage.getItem('name')){
+//     setUserName();
+// }else{
+//     let storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Introducing Loki to, '+ storedName;
+// }
+
+// myButton.onclick = function(){
+//     setUserName();
+// }
